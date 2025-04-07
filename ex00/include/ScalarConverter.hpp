fb22032615ee6735ce45f048aecb8ce154dc5e4b
ekenln <eklund.ellen.ee@gmail.com>
2025-04-07 18:03:40 +0200

    ex00 start

---
+++ ex00/include/ScalarConverter.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScalarConverter.hpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: eeklund <eeklund@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/07 10:12:57 by eeklund       #+#    #+#                 */
+/*   Updated: 2025/04/07 17:22:31 by eeklund       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class ScalarConverter
+{
+	static void	convert(std::string string);
+};
---
+++ ex00/src/ScalarConverter.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScalarConverter.cpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: eeklund <eeklund@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/07 17:22:33 by eeklund       #+#    #+#                 */
+/*   Updated: 2025/04/07 17:30:12 by eeklund       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+void	ScalarConverter::convert(std::string string)
+{
+	//identify type and convert from string to that type
+	//convert to all the other types
+	//overflows or any weird thing --> msg saying cenveriosn is impossible
+	//display result
+	/*types::
+	-char
+	-int
+	-float
+	-double 
+	*/
+}
