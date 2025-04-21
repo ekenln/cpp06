bb73905ec2ec0be4613c8deda4fc7a01a958d574
ellen eklund <elleneklund@MacBook-Air-69>
2025-04-21 18:52:19 +0200

    cleaned ex00

--- ex00/include/ScalarConverter.hpp
+++ ex00/include/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/07 10:12:57 by eeklund       #+#    #+#                 */
-/*   Updated: 2025/04/18 09:59:33 by elleneklund   ########   odam.nl         */
+/*   Updated: 2025/04/21 18:51:33 by elleneklund   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,37 @@ class ScalarConverter
 	public:
 		static void	convert(const std::string& string);
 };
+
+template <typename T> void generalConverter(T type)
+{
+	int		integer = 0;
+	bool	impossibleChar = false;
+	bool	impossibleInt = false;
+
+	float floatValue = static_cast<float>(type);
+	double doubleValue = static_cast<double>(type);
+	char character = static_cast<char>(type);
+	
+	if (!std::isprint(static_cast<unsigned char>(character)))
+		impossibleChar = true;
+	if (type > INT_MAX || type < INT_MIN)
+		impossibleInt = true;
+	else
+		integer = static_cast<int>(type);
+
+	std::cout << "char: ";
+	if (impossibleChar)
+		std::cout << "Non displayable\n";
+	else
+		std::cout << '\'' << character << "\'\n";
+
+	std::cout << "int: ";
+	if (impossibleInt)
+		std::cout << "Impossible\n";
+	else
+		std::cout << integer << "\n";
+
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "float: " << floatValue << "f\n";
+	std::cout << "double: " << doubleValue << "\n";
+}
--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -6,82 +6,101 @@
 /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/07 17:22:33 by eeklund       #+#    #+#                 */
-/*   Updated: 2025/04/18 10:15:23 by elleneklund   ########   odam.nl         */
+/*   Updated: 2025/04/21 18:47:07 by elleneklund   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include "detectorFuncs.hpp"
 
+// void	ScalarConverter::convert(const std::string& str)
+// {
+// 	if (str.empty())
+// 		return ;
+
+// 	int		integer = 0;
+// 	char	character = 0;
+// 	float	floatValue = 0.0f;
+// 	double	doubleValue = 0.0;
+// 	bool	impossibleChar = false;
+// 	bool	impossibleInt = false;
+
+// 	if (isInt(str))
+// 	{
+// 		integer = std::stoi(str);
+// 		character = static_cast<char>(integer);
+// 		if (!std::isprint(static_cast<unsigned char>(character)))
+// 			impossibleChar = true;
+// 		floatValue = static_cast<float>(integer);
+// 		doubleValue = static_cast<double>(integer);
+// 	}
+// 	else if (isChar(str))
+// 	{
+// 		character = str[0];
+// 		integer = static_cast<int>(character);
+// 		floatValue = static_cast<float>(character);
+// 		doubleValue = static_cast<double>(character);
+// 	}
+// 	else if (isDouble(str))
+// 	{
+// 		doubleValue = std::stod(str);
+// 		floatValue = static_cast<float>(doubleValue);
+// 		character = static_cast<char>(doubleValue);
+// 		if (!std::isprint(static_cast<unsigned char>(character)))
+// 			impossibleChar = true;
+// 		if (doubleValue > INT_MAX || doubleValue < INT_MIN)
+// 			impossibleInt = true;
+// 		else
+// 			integer = static_cast<int>(doubleValue);
+		
+// 	}
+// 	else if (isFloat(str))
+// 	{
+// 		floatValue = std::stof(str);
+// 		doubleValue = static_cast<double>(floatValue);
+// 		character = static_cast<char>(floatValue);
+// 		if (!std::isprint(static_cast<unsigned char>(character)))
+// 			impossibleChar = true;
+// 		if (floatValue > INT_MAX || floatValue < INT_MIN)
+// 			impossibleInt = true;
+// 		else
+// 			integer = static_cast<float>(floatValue);
+// 	}
+// 	else
+// 	{
+// 		std::cerr << "Not a literal\n";
+// 	}
+
+// 	std::cout << "char: ";
+// 	if (impossibleChar)
+// 		std::cout << "impossibleChar\n";
+// 	else
+// 		std::cout << character << "\n";
+// 	std::cout << "int: ";
+// 	if (impossibleInt)
+// 		std::cout << "impossible\n";
+// 	else
+// 		std::cout << integer << "\n";
+// 	std::cout << std::fixed << std::setprecision(1);
+// 	std::cout << "float: " << floatValue << "f\n";
+// 	std::cout << "double: " << doubleValue << "\n";
+// }
+
 void	ScalarConverter::convert(const std::string& str)
 {
 	if (str.empty())
 		return ;
 
-	int		integer = 0;
-	char	character = 0;
-	float	floatValue = 0.0f;
-	double	doubleValue = 0.0;
-	bool	impossibleChar = false;
-	bool	impossibleInt = false;
-
 	if (isInt(str))
-	{
-		integer = std::stoi(str);
-		character = static_cast<char>(integer);
-		if (!std::isprint(static_cast<unsigned char>(character)))
-			impossibleChar = true;
-		floatValue = static_cast<float>(integer);
-		doubleValue = static_cast<double>(integer);
-	}
+		::generalConverter<int>(std::stoi(str));
 	else if (isChar(str))
-	{
-		character = str[0];
-		integer = static_cast<int>(character);
-		floatValue = static_cast<float>(character);
-		doubleValue = static_cast<double>(character);
-	}
+		::generalConverter<char>(str[0]);
 	else if (isDouble(str))
-	{
-		doubleValue = std::stod(str);
-		floatValue = static_cast<float>(doubleValue);
-		character = static_cast<char>(doubleValue);
-		if (!std::isprint(static_cast<unsigned char>(character)))
-			impossibleChar = true;
-		if (doubleValue > INT_MAX || doubleValue < INT_MIN)
-			impossibleInt = true;
-		else
-			integer = static_cast<int>(doubleValue);
-		
-	}
+		::generalConverter<double>(std::stod(str));
 	else if (isFloat(str))
-	{
-		floatValue = std::stof(str);
-		doubleValue = static_cast<double>(floatValue);
-		character = static_cast<char>(floatValue);
-		if (!std::isprint(static_cast<unsigned char>(character)))
-			impossibleChar = true;
-		if (floatValue > INT_MAX || floatValue < INT_MIN)
-			impossibleInt = true;
-		else
-			integer = static_cast<float>(floatValue);
-	}
+		::generalConverter<float>(std::stof(str));
 	else
 	{
 		std::cerr << "Not a literal\n";
 	}
-
-	std::cout << "char: ";
-	if (impossibleChar)
-		std::cout << "impossibleChar\n";
-	else
-		std::cout << character << "\n";
-	std::cout << "int: ";
-	if (impossibleInt)
-		std::cout << "impossible\n";
-	else
-		std::cout << integer << "\n";
-	std::cout << std::fixed << std::setprecision(1);
-	std::cout << "float: " << floatValue << "f\n";
-	std::cout << "double: " << doubleValue << "\n";
 }
